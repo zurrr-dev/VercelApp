9e2a52af0473623b8ab97ca0ec688c4490c298c1
SenaCrasher <asrafael255@gmail.com>
2025-05-07 23:37:09 +0700

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Portfolio-AlwaysZuroku
+nothing 
---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Halo!!", "It`sMe AlwaysZuroku :","Pembahasan Tentang","JavaScript Yang Di Gunakan","Untuk Membuat Bot WhatsApp","Dengan Jangka Waktu","Yang Tidak Di Tentukan","Pecinta Bahasa Pemrograman / Orang","Yang Memahami Tentang Bahasa Pemrograman","Akan Faham Tentang Beberapa","Methode Dalam Ilmu Tersebut","Contoh Saja : ","JavaScript, HTML, Css, Python Dll"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 70,
+    loop: true
+});
